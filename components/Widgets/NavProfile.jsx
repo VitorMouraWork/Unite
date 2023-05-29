4900b44c36a73e34ed0daa66f72bb7805baad73c
Vitor D <105220321+VitorMouraWork@users.noreply.github.com>
2023-05-29 11:52:21 -0300

    Update NavProfile.jsx

--- components/Widgets/NavProfile.jsx
+++ components/Widgets/NavProfile.jsx
@@ -1,78 +1,61 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import { signOut, useSession } from "next-auth/react";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 const NavProfile = () => {
   const { data: session } = useSession();
   const router = useRouter();
-  const [loading, setLoading] = useState(false); // Added loading state
+  const [loading, setLoading] = useState(false);
 
   const handleSignOut = async () => {
-    setLoading(true); // Set loading state to true
-    await signOut({ redirect: false, callbackUrl: "/" }); // Calls the signOut function from NextAuth
-    setLoading(false); // Set loading state to false
-    await router.push("/"); // Redirects to the home page after sign-out
+    setLoading(true);
+    await signOut({ redirect: false, callbackUrl: "/" });
+    setLoading(false);
+    await router.push("/");
   };
 
-  useEffect( async () => {
-    // You can add any additional logic here to handle the sign-out process
-    // For example, clearing local storage, resetting app state, etc.
+  useEffect(() => {
     if (!session && !loading) {
-//      await router.push("/"); // Redirects to the home page if there is no active session and not in a loading state
+      router.push("/");
     }
   }, [session, router, loading]);
+
+  if (!session) {
+    return null; // or render a loading indicator
+  }
 
   return (
     <div className="flex bg-white dark:bg-neutral-800 rounded-full cursor-pointer ease-out duration-200">
       <div className="flex">
-        {session?.user ? (
-          <Image
-            src={session.user.image}
-            alt="Profile"
-            width={35}
-            height={35}
-            className="rounded-full m-1"
-          />
-        ) : (
-          <Image
-            src="/assets/images/logo.svg"
-            alt="Profile"
-            width={35}
-            height={35}
-            className="rounded-full m-1"
-          />
-        )}
-        {session?.user ? (
-          <div className="flex flex-col">
-            <p className="ease-out duration-200 hover:text-unite-orange text-neutral-600 dark:text-neutral-400">
-              {session.user.name}
-            </p>
-            <p className="text-xs text-neutral-400 dark:text-neutral-600 leading-3">
-              @{session.user.name}
-            </p>
-          </div>
-        ) : (
-          <div className="flex flex-col">
-            <p className="ease-out duration-200 hover:text-unite-orange text-neutral-600 dark:text-neutral-400">
-              Nickname
-            </p>
-            <p className="text-xs text-neutral-400 dark:text-neutral-600 leading-3">
-              @Username
-            </p>
-          </div>
-        )}
+        <Image
+          src={session.user?.image || "/assets/images/logo.svg"}
+          alt="Profile"
+          width={35}
+          height={35}
+          className="rounded-full m-1"
+        />
+        <div className="flex flex-col">
+          <p className="ease-out duration-200 hover:text-unite-orange text-neutral-600 dark:text-neutral-400">
+            {session.user?.name || "Nickname"}
+          </p>
+          <p className="text-xs text-neutral-400 dark:text-neutral-600 leading-3">
+            @{session.user?.name || "Username"}
+          </p>
+        </div>
       </div>
 
-      {session?.user && <button onClick={handleSignOut} href="/">
-        <Image
+      {session.user && (
+        <button onClick={handleSignOut} href="/">
+          <Image
             src="/assets/icons/logout.svg"
             alt="Profile"
             width={20}
             height={20}
             className="ml-4"
           />
-      </button>}
+        </button>
+      )}
     </div>
   );
 };
