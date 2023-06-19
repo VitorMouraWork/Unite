0ae72ef33e351b76daeb09c7a52712e77e0715ca
Vitor D <12100471@aluno.cotemig.com.br>
2023-06-19 19:33:45 -0300

    LandingPage Fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,13 @@
 'use client';
 
-import Register from '@components/LandingPage';
 import { ThemeProvider } from 'next-themes';
 
 const LandingPage = () => {
   return (
     <ThemeProvider enableSystem={true} attribute='class'>
-      <LandingPage/>
+      <div>
+        <h1>Landing Page</h1>
+      </div>
     </ThemeProvider>
   )
 }
