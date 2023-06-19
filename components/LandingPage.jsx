6684e43a7ea19486064e32f78d865e90fe7d804b
Vitor D <12100471@aluno.cotemig.com.br>
2023-06-19 13:14:41 -0300

    Update LandingPage.jsx

---
+++ components/LandingPage.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import '@styles/globals.css';
+
+const LandingPage = () => {
+
+    return (
+        //seu código ficará aqui
+        <div class="flex bg-white dark:bg-neutral-800 rounded-xl flex-col items-center ease-out duration-200 py-2">
+            <h1> Landing Page </h1> 
+        </div>
+    )
+
+};
+
+export default LandingPage;
