66e7d5c7583b706f0eb83f2925e6c2da76a7e03e
Vitor D <105220321+VitorMouraWork@users.noreply.github.com>
2023-07-07 08:54:52 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
       },
       colors: {
         'unite-orange': '#F24333',
+      },
+      backgroundImage: {
+        'unite-blob': "url('/public/assets/images/bg.png')",
+        'footer-texture': "url('/img/footer-texture.png')",
       }
     },
   },
